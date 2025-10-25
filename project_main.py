cfab359b6596a1ba32d1c6877effc289f3f4edd9
Thomas George Panicker <gpthomas101@gmail.com>
2025-10-25 18:12:40 +0530

    initial commit and venv setup

---
+++ project_main.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import Dataset,DataLoader 
+import torchvision
+import pandas as pd
+import numpy as np
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import matplotlib
+import torch.nn as nn
+import torch.cuda as cuda
+
+#printing versions for future reference
+
+print(f'''  Versions
+Torch: {torch.__version__}
+Torchvision: {torchvision.__version__}
+Numpy: {np.__version__}
+Matplotlib: {matplotlib.__version__}
+''')
+
+if cuda.is_available():
+
+    DEFAULT_DEVICE="cuda"
+    cuda.manual_seed_all(120)
+    torch.manual_seed(120)
+
+else:
+
+    DEFAULT_DEVICE="cpu"
+    torch.manual_seed(120)
+
+transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.2860,), (0.3530,)),transforms.Resize((512,512))])
+train_dataset=torchvision.datasets.ImageFolder("./train/",transform=transform,)
+
+print(type(train_dataset))
